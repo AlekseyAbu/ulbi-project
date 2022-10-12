7448de0e6eff7f0ceac77202789c52c0c3543b07
Aleksey <direct.abu@yandex.ru>
2022-10-12 17:05:19 +0300

    add eslint, fix all files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,46 +1,26 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React, { Suspense } from 'react';
 import './styles/index.scss';
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AppRouter} from "app/providers/router";
-import {Navbar} from "widgets/Navbar";
-import {Sidebar} from "widgets/Sidebar";
-import {useTranslation} from "react-i18next";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 
-const Component = () => {
-  const { t, i18n } = useTranslation();
-
-  const toggle = () => {
-    i18n.changeLanguage((i18n.language === 'ru' ? 'en' : 'ru'))
-  }
-
-  return(
-    <div>
-      <button onClick={toggle}>{t('translate')}</button>
-      <div>{t('testPrimer')}</div>
-    </div>
-  )
-}
-
-
-const App = () => {
-  const {theme} = useTheme();
-
-  return (
-    <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback="">
-        <Component />
-        <Navbar/>
-        <div className="content-page">
-          <Sidebar/>
-          <AppRouter/>
+function App() {
+    const { theme } = useTheme();
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <LangSwitcher />
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
-      </Suspense>
-    </div>
-  );
-};
+    );
+}
 
 export default App;
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,27 +1,27 @@
-import cls from './Sidebar.module.scss'
-import {classNames} from "shared/lib/classNames/classNames";
-import React, {useState} from "react";
-import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import { classNames } from 'shared/lib/classNames/classNames';
+import React, { useState } from 'react';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
   className?: string,
 }
 
-export const Sidebar = ({className}: SidebarProps) => {
-  const [collapsed, setCollapsed] = useState(false);
+export const Sidebar = ({ className }: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
 
-  const onToggle = () => {
-    setCollapsed(prev => !prev)
-  }
+    const onToggle = () => {
+        setCollapsed((prev) => !prev);
+    };
 
-  return (
-    <div
-      className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
-    >
-      <div className={cls.switchers}>
-        <ThemeSwitcher />
-      </div>
+    return (
+        <div
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+        >
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+            </div>
 
-    </div>
-  );
+        </div>
+    );
 };
