386b6df90202600f6e52facfd5c6a8f7a09b5b67
Aleksey <direct.abu@yandex.ru>
2022-10-25 18:36:02 +0300

    add modal, portal

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Modal } from 'shared/ui/Modal/Modal';
+
+export default {
+    title: 'shared/Modal',
+    component: Modal,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Modal>;
+
+const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    isOpen: true,
+    children: 'wdlkfnwkjefnkwjefn kjwen fkdjwe nfkdj wkjdf ckwjen fdkjw ekf wkej fckwe ck wejkhcfbwejkhbciwencfiwe',
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -11,11 +12,26 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t, i18n } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('sighIn')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                {/* eslint-disable-next-line */}
+                wdlkfnwkjefnkwjefn kjwen fkdjwe nfkdj wkjdf ckwjen fdkjw ekf wkej fckwe ck wejkhcfbwejkhbciwencfiwe
+            </Modal>
         </div>
     );
 };
