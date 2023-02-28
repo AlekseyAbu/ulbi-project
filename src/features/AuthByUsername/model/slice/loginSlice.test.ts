1c7de3d5650d62ea891a643a9acd61aa03284b7b
Aleksey <direct.abu@yandex.ru>
2023-02-28 18:20:05 +0300

    add tests for login module

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -8,13 +8,13 @@ describe('loginSlice.test', () => {
         expect(loginReducer(
             state as LoginSchema,
             loginActions.setUsername('123123'),
-        )).toEqual({ username: '123' });
+        )).toEqual({ username: '123123' });
     });
     test('test set password', () => {
         const state: DeepPartial<LoginSchema> = { password: '123' };
         expect(loginReducer(
             state as LoginSchema,
             loginActions.setPassword('123123'),
-        )).toEqual({ password: '123' });
+        )).toEqual({ password: '123123' });
     });
 });
