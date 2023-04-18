ff4d575e53974feaac2565634012386262f9e0fc
Aleksey <direct.abu@yandex.ru>
2023-04-18 19:11:03 +0300

    add comments list

---
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -0,0 +1,45 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Text } from 'shared/ui/Text/Text';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import cls from './CommentCard.module.scss';
+import { Comment } from '../../model/types/comment';
+
+interface CommentCardProps {
+    className?: string,
+    comment: Comment,
+    isLoading?: boolean;
+}
+
+export const CommentCard = memo((props: CommentCardProps) => {
+    const {
+        className,
+        comment,
+        isLoading,
+    } = props;
+    const { t } = useTranslation();
+
+    if (isLoading) {
+        return (
+            <div className={classNames(cls.CommentCard, {}, [className])}>
+                <div className={cls.header}>
+                    <Skeleton width={30} height={30} border="50%" />
+                    <Skeleton height={16} width={10} className={cls.username} />
+                </div>
+                <Skeleton height={50} width="100%" className={cls.text} />
+            </div>
+        );
+    }
+
+    return (
+        <div className={classNames(cls.CommentCard, {}, [className])}>
+            <div className={cls.header}>
+                { comment.user.avatar ? <Avatar size={30} src={comment.user.avatar} /> : null }
+                <Text className={cls.username} title={comment.user.username} />
+            </div>
+            <Text className={cls.text} text={comment.text} />
+        </div>
+    );
+});
--- src/pages/ArticleDetailePage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailePage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -3,16 +3,36 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { memo } from 'react';
 import { useParams } from 'react-router-dom';
 import { ArticleDetails } from 'entities/Article';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentList } from 'entities/Comment';
+import { DynamicModuleLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useSelector } from 'react-redux';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { fetchCommentsByArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+import { articleDetailsCommentsReducer, getArticleComments } from '../../model/slices/ArticleDetailsCommentsSlice';
 import cls from './ArticleDetailsPage.module.scss';
+import { getArticleCommentIsLoading } from '../../model/selectors/comment';
 
 interface ArticleDetailsPageProps {
     className?: string,
 }
 
+const reducers: ReducerList = {
+    articleDetailsComments: articleDetailsCommentsReducer,
+};
+
 const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     const { className } = props;
     const { t } = useTranslation('article');
     const { id } = useParams<{ id: string }>();
+    const dispatch = useAppDispatch();
+    const comments = useSelector((getArticleComments.selectAll));
+    const commentsIsLoading = useSelector((getArticleCommentIsLoading));
+
+    useInitialEffect(() => {
+        dispatch(fetchCommentsByArticleId(id));
+    });
 
     if (!id) {
         return (
@@ -23,11 +43,15 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     }
 
     return (
-        <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-            <ArticleDetails
-                id={id}
-            />
-        </div>
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+                <ArticleDetails
+                    id={id}
+                />
+                <Text title={t('Комментарии')} className={cls.commentTitle} />
+                <CommentList comments={comments} isLoading={commentsIsLoading} />
+            </div>
+        </DynamicModuleLoader>
     );
 };
 
